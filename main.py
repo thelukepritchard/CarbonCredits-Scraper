90c376f383200528200c20e9a8b234ad54053eff
Luke Pritchard <me@lpritchard.com>
2024-04-03 21:56:12 +1000

    Intial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import time
+
+def get_website_title(url):
+
+  # Specify the path to your WebDriver executable
+  driver_path = './mac_arm64_chromedriver'  # Change this to the path of your WebDriver
+  
+  # Initialize the WebDriver (this example uses Chrome)
+  service = Service(executable_path=driver_path)
+  options = webdriver.ChromeOptions()
+  options.add_argument('--headless')  # Enable headless mode for no GUI
+  driver = webdriver.Chrome(service=service, options=options)
+    
+  try:
+    # Open the website
+    driver.get(url)
+    time.sleep(2)  # Wait for the page to load
+    
+    # Parse the page source with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    # Find the table with the aria-label attribute set to 'Live Carbon Prices'
+    table = soup.find('table', {'aria-label': 'Live Carbon Prices'})
+    
+    # Get the body from the table
+    tbody = table.find('tbody')
+      
+    # Iterate through each tr within the tbody
+    for row in tbody.find_all('tr'):
+            
+        # Skip the header rows
+        if row.find('td').text == 'Compliance Markets' or row.find('td').text == 'Voluntary Markets':
+          continue
+        
+        # Extract the data from the row
+        market = row.find_all('td')[0].text.strip()
+        price = row.find_all('td')[1].text.strip()
+        change = row.find_all('td')[2].text.strip()
+        ytd = row.find_all('td')[3].text.strip()
+ 
+        # Print the data
+        print(f'{market}: {price}')
+
+  finally:
+    driver.quit()  # Make sure to quit the driver to free resources
+
+# Start the script
+url = 'https://carboncredits.com/carbon-prices-today/'
+get_website_title(url)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Carbon Credits Scraper
+
+This script simply scrapes the site carboncredits.com and extracts the market data
+
+## Usage
+
+```sh
+python3 main.py
+```
+
+## Install chromedriver
+
+You will need apppropriate chromedriver from
+https://googlechromelabs.github.io/chrome-for-testing/#stable
+
+You need to install appropaite chromedriver
+
+## Install PIP packages
+
+```sh
+pip3 install -r requirements.txt
+```
